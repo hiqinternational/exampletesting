f2aa253d5b5d4392a9a6d2c868e7ffb20b3f415c
hiqinternational <51332507+hiqinternational@users.noreply.github.com>
2019-06-05 15:25:45 +0800

    Update gcexample1.go

--- gcexample1.go
+++ gcexample1.go
@@ -1,31 +1,46 @@
 package main
 
-import (  
-    "fmt"  
-    "math/rand"  
-    "runtime"  
+import (
+    "fmt"
+    "math/rand"
+    "runtime"
     "time"
 )
 
-func makeBuffer() []byte {  
-    return make([]byte, rand.Intn(5000000)+5000000)  
+func makeBuffer() []byte {
+    return make([]byte, rand.Intn(5000000)+5000000)
 }
 
-func main() {  
+func main() {
     pool := make([][]byte, 20)
 
-    var m runtime.MemStats  
-    makes := 0  
-    for {  
-        b := makeBuffer()  
-        makes += 1
+    buffer := make(chan []byte, 5)
+
+    var m runtime.MemStats
+    makes := 0
+    for {
+        var b []byte
+        select {
+        case b = <-buffer:
+        default:
+            makes += 1
+            b = makeBuffer()
+        }
+
         i := rand.Intn(len(pool))
+        if pool[i] != nil {
+            select {
+            case buffer <- pool[i]:
+                pool[i] = nil
+            default:
+            }
+        }
+
         pool[i] = b
 
         time.Sleep(time.Second)
 
         bytes := 0
-
         for i := 0; i < len(pool); i++ {
             if pool[i] != nil {
                 bytes += len(pool[i])
